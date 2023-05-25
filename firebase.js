d8946bd2e7f590a4296bd827cde5ec8a60826ac7
Agrim <agrimsharma1714@gmail.com>
2023-05-26 00:17:33 +0530

    added the loading screen

--- firebase.js
+++ firebase.js
@@ -2,20 +2,29 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyBrBUBvMzpnowsFEMq0Ti1AG7Rpkl00FoQ",
+  authDomain: "agrim-chat.firebaseapp.com",
+  projectId: "agrim-chat",
+  storageBucket: "agrim-chat.appspot.com",
+  messagingSenderId: "173097816538",
+  appId: "1:173097816538:web:11b69ccad9a2e2cb5770e3",
 };
 
-const app = initializeApp(firebaseConfig);
+let app;
+
+try {
+  app = initializeApp(firebaseConfig);
+} catch (error) {
+  if (!/already exists/.test(error.message)) {
+    console.error("Firebase initialization error", error.stack);
+  }
+}
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider(app);
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import Login from '@/components/Login'
 import Sidebar from '@/components/Sidebar'
+import Loading from '@/components/Loading'
+import { auth } from '@/firebase';
 import '@/styles/globals.css'
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function App({ Component, pageProps }) {
-  if(true) return <Login />
+
+  const [user, loading] = useAuthState(auth);
+
+  if(!user) return <Login />
+  if(loading) return <Loading />
+
   return (
     <div className='flex'>
     <Sidebar />
