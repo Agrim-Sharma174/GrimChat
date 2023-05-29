aee6b7a38f3435ad5da24d20afde76e53c8885dd
Agrim <agrimsharma1714@gmail.com>
2023-05-29 18:02:49 +0530

    working almost done, everything seems fine

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,12 +1,27 @@
-import Image from 'next/image'
-import React from 'react'
+// import Image from 'next/image'
+// import React from 'react'
  
-const Loadng = () => {
+// const Loadng = () => {
+//   return (
+//     <div className='flex justify-center items-center w-full h-screen bg-[#161621]'>
+//       <Image src={"/gifs/Loading.gif"} alt="" width={800} height={600} />
+//     </div>
+//   )
+// }
+ 
+// export default Loadng
+
+import React from 'react'
+import Image from "next/image";
+import Loader from "../public/gifs/Loading.gif"
+
+
+const Loading = () => {
   return (
-    <div className='flex justify-center items-center w-full h-screen bg-[#161621]'>
-      <Image src={"/gifs/Loading.gif"} alt="" width={800} height={600} />
+    <div className='flex justify-center items-center w-full h-screen overflow-hidden bg-[#161621]'>
+        <Image src={Loader} width={200} height={200} className="w-full" alt="" priority={true} quality={100} />
     </div>
   )
 }
- 
-export default Loadng
+
+export default Loading
--- pages/chats/[id].js
+++ pages/chats/[id].js
@@ -1,133 +1,133 @@
-import Message from "@/components/Message";
-import Image from "next/image";
-import React, { useState } from "react";
-import { MdSend } from "react-icons/md";
-import {
-  collection,
-  addDoc,
-  serverTimestamp,
-  orderBy,
-  query,
-  where,
-} from "firebase/firestore";
-import { auth, db } from "@/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore";
-import { doc, getDoc } from "firebase/firestore";
+// import Message from "@/components/Message";
+// import Image from "next/image";
+// import React, { useState } from "react";
+// import { MdSend } from "react-icons/md";
+// import {
+//   collection,
+//   addDoc,
+//   serverTimestamp,
+//   orderBy,
+//   query,
+//   where,
+// } from "firebase/firestore";
+// import { auth, db } from "@/firebase";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { useCollection } from "react-firebase-hooks/firestore";
+// import { doc, getDoc } from "firebase/firestore";
 
-export async function getServerSideProps(context) {
-  const id = context.query.id;
-  const docRef = doc(db, "chats", id);
-  const docSnap = await getDoc(docRef);
-  // const chatData = JSON.stringify(docSnap?.data());
-  const chatData = docSnap?.data() ? JSON.stringify(docSnap.data()) : null;
+// export async function getServerSideProps(context) {
+//   const id = context.query.id;
+//   const docRef = doc(db, "chats", id);
+//   const docSnap = await getDoc(docRef);
+//   // const chatData = JSON.stringify(docSnap?.data());
+//   const chatData = docSnap?.data() ? JSON.stringify(docSnap.data()) : null;
 
-  return {
-    props: {
-      id,
-      chatData,
-    }, // will be passed to the page component as props
-  };
-}
+//   return {
+//     props: {
+//       id,
+//       chatData,
+//     }, // will be passed to the page component as props
+//   };
+// }
 
-const Id = ({ id, chatData }) => {
-  const [message, setMessage] = useState("");
-  const [user, loading] = useAuthState(auth);
+// const Id = ({ id, chatData }) => {
+//   const [message, setMessage] = useState("");
+//   const [user, loading] = useAuthState(auth);
 
-  const messagesRef = collection(db, "messages");
-  const q = query(messagesRef, orderBy("createdAt"));
-  const [messageSnapshots, loading2] = useCollection(q);
+//   const messagesRef = collection(db, "messages");
+//   const q = query(messagesRef, orderBy("createdAt"));
+//   const [messageSnapshots, loading2] = useCollection(q);
 
-  const createMessage = async (e) => {
-    e.preventDefault();
-    // this below code do make a collection of messages  and then we import db means database then inside the func. we giving messages
-    const docRef = await addDoc(collection(db, "messages"), {
-      message: message,
-      user: user?.email,
-      chatId: id,
-      createdAt: serverTimestamp(),
-    });
-    setMessage("");
-  };
+//   const createMessage = async (e) => {
+//     e.preventDefault();
+//     // this below code do make a collection of messages  and then we import db means database then inside the func. we giving messages
+//     const docRef = await addDoc(collection(db, "messages"), {
+//       message: message,
+//       user: user?.email,
+//       chatId: id,
+//       createdAt: serverTimestamp(),
+//     });
+//     setMessage("");
+//   };
 
-  let receiverEmail, name, imageURL, online, lastSeen, time, date;
+//   let receiverEmail, name, imageURL, online, lastSeen, time, date;
 
-  if (chatData) {
-    const data = JSON.parse(chatData);
-    receiverEmail = data?.users?.filter((item) => item !== user?.email)?.[0];
-    // console.log(receiverEmail);
-    const usersRef = collection(db, "users");
-    const q2 = query(usersRef, where("email", "==", receiverEmail));
-    const [userSnapShot, loading3] = useCollection(q2);
-    name = userSnapShot?.docs?.[0]?.data()?.name;
-    imageURL = userSnapShot?.docs?.[0]?.data()?.imageURL;
-    online = userSnapShot?.docs?.[0]?.data()?.online;
-    lastSeen = userSnapShot?.docs?.[0]?.data()?.lastSeen;
-    const newDate = new Date(lastSeen?.seconds * 1000);
-    time = newDate.toLocaleTimeString().slice(0, 5);
-    date = newDate.toLocaleDateString();
-  }
+//   if (chatData) {
+//     const data = JSON.parse(chatData);
+//     receiverEmail = data?.users?.filter((item) => item !== user?.email)?.[0];
+//     // console.log(receiverEmail);
+//     const usersRef = collection(db, "users");
+//     const q2 = query(usersRef, where("email", "==", receiverEmail));
+//     const [userSnapShot, loading3] = useCollection(q2);
+//     name = userSnapShot?.docs?.[0]?.data()?.name;
+//     imageURL = userSnapShot?.docs?.[0]?.data()?.imageURL;
+//     online = userSnapShot?.docs?.[0]?.data()?.online;
+//     lastSeen = userSnapShot?.docs?.[0]?.data()?.lastSeen;
+//     const newDate = new Date(lastSeen?.seconds * 1000);
+//     time = newDate.toLocaleTimeString().slice(0, 5);
+//     date = newDate.toLocaleDateString();
+//   }
   
 
-  return (
-    <div className="gradient w-full h-screen overflow-hidden">
-      <div className="w-full p-5 bg-[#00000044] backdrop-blur-sm flex items-center space-x-5">
-        <div>
-          <Image
-            src={imageURL || "/images/default.png"}
-            width={70}
-            height={70}
-            className="rounded-full"
-            alt=""
-          />
-        </div>
-        <div>
-          <div>{name}</div>
-          <div>
-            last seen at {time || ""} on {date || ""}
-          </div>
-        </div>
-      </div>
-      <div className="w-full h-[78vh] overflow-y-auto overflow-x-hidden p-5">
-        {messageSnapshots?.docs?.map((msg) => {
-          return (
-            <div
-              className={
-                msg.data().user === user?.email
-                  ? "w-full flex justify-end"
-                  : "w-full flex"
-              }
-            >
-              <Message msg={msg} />
-            </div>
-          );
-        })}
-      </div>
-      <form
-        onSubmit={createMessage}
-        className="w-full p-5 lg:p-0 bg-[#00000044] backdrop-blur-sm h-full"
-      >
-        <div className="flex items-center relative">
-          <input
-            type="text"
-            className="w-full border pr-10 pl-5 py-4 bg-transparent rounded-full outline-none focus:border-[#00ad03]"
-            placeholder="Type Here"
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-            value={message}
-            required
-          />
-          <button className="text-3xl absolute right-4">
-            <MdSend />
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div className="gradient w-full h-screen overflow-hidden">
+//       <div className="w-full p-5 bg-[#00000044] backdrop-blur-sm flex items-center space-x-5">
+//         <div>
+//           <Image
+//             src={imageURL || "/images/default.png"}
+//             width={70}
+//             height={70}
+//             className="rounded-full"
+//             alt=""
+//           />
+//         </div>
+//         <div>
+//           <div>{name}</div>
+//           <div>
+//             last seen at {time || ""} on {date || ""}
+//           </div>
+//         </div>
+//       </div>
+//       <div className="w-full h-[78vh] overflow-y-auto overflow-x-hidden p-5">
+//         {messageSnapshots?.docs?.map((msg) => {
+//           return (
+//             <div
+//               className={
+//                 msg.data().user === user?.email
+//                   ? "w-full flex justify-end"
+//                   : "w-full flex"
+//               }
+//             >
+//               <Message msg={msg} />
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <form
+//         onSubmit={createMessage}
+//         className="w-full p-5 lg:p-0 bg-[#00000044] backdrop-blur-sm h-full"
+//       >
+//         <div className="flex items-center relative">
+//           <input
+//             type="text"
+//             className="w-full border pr-10 pl-5 py-4 bg-transparent rounded-full outline-none focus:border-[#00ad03]"
+//             placeholder="Type Here"
+//             onChange={(e) => {
+//               setMessage(e.target.value);
+//             }}
+//             value={message}
+//             required
+//           />
+//           <button className="text-3xl absolute right-4">
+//             <MdSend />
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
 
-export default Id;
+// export default Id;
 
 
 //! UNKA WALA
@@ -332,3 +332,135 @@ export default Id;
 // };
 
 // export default Id;
+
+import React, { useState } from "react";
+import Image from "next/image";
+import DefaultImage from "../../public/images/default.png";
+import Message from "../../components/Message";
+import { MdSend } from "react-icons/md";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  orderBy,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { doc, getDoc } from "firebase/firestore";
+
+export async function getServerSideProps(context) {
+  const id = context.query.id;
+  const docRef = doc(db, "chats", id);
+  const docSnap = await getDoc(docRef);
+  const chatData = JSON.stringify(docSnap?.data());
+
+  return {
+    props: {
+      id,
+      chatData,
+    }, // will be passed to the page component as props
+  };
+}
+
+const Id = ({ id, chatData }) => {
+  const [message, setMessage] = useState("");
+  const [user, loading] = useAuthState(auth);
+
+  const messagesRef = collection(db, "messages");
+  const q = query(messagesRef, orderBy("createdAt"));
+  const [messageSnapshots, loading2] = useCollection(q);
+
+  const createMessage = async (e) => {
+    e.preventDefault();
+    const docRef = await addDoc(collection(db, "messages"), {
+      message: message,
+      user: user?.email,
+      chatId: id,
+      createdAt: serverTimestamp(),
+    });
+    setMessage("");
+  };
+  const data = JSON.parse(chatData);
+  const reciverEmail = data?.users?.filter((item) => item !== user?.email)?.[0];
+  // console.log(reciverEmail);
+  const usersRef = collection(db, "users");
+  const q2 = query(usersRef, where("email", "==", reciverEmail));
+  const [userSnapShot, loading3] = useCollection(q2);
+  const name = userSnapShot?.docs?.[0]?.data()?.name;
+  const imageURL = userSnapShot?.docs?.[0]?.data()?.imageURL;
+  const online = userSnapShot?.docs?.[0]?.data()?.online;
+  const lastSeen = userSnapShot?.docs?.[0]?.data()?.lastSeen;
+  const newDate = new Date(lastSeen?.seconds * 1000);
+  const time = newDate.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  const date = newDate.toLocaleDateString();
+
+  return (
+    <div className="gradient w-full h-screen overflow-hidden">
+      <div className="w-full p-5 bg-[#00000044] backdrop-blur-sm flex items-center space-x-5">
+        <div>
+          <Image
+            src={imageURL || DefaultImage}
+            width={70}
+            height={70}
+            priority={true}
+            quality={100}
+            alt=""
+            className="rounded-full"
+          />
+        </div>
+        <div>
+          <div>{name}</div>
+          <div>
+            last seen at {time || ""} on {date || ""}
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-[74vh] md:h-[75vh] overflow-y-auto overflow-x-hidden p-5">
+        {messageSnapshots?.docs?.map((msg) => {
+          if (msg.data().chatId === id) {
+            return (
+              <div key={msg.id}
+                className={
+                  msg.data().user === user?.email
+                    ? "w-full flex justify-end mb-5"
+                    : "w-full flex mb-5"
+                }
+              >
+                <Message msg={msg} />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <form
+        onSubmit={createMessage}
+        className="w-full bg-[#00000044] backdrop-blur-sm h-full md:-mt-1"
+      >
+        <div className="flex items-center relative">
+          <input
+            type="text"
+            className="w-full h-14 border pr-10 pl-5 bg-transparent rounded-full outline-none focus:border-[#ffee32]"
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            value={message}
+            placeholder="Type Here"
+            required
+          />
+          <button className="text-3xl absolute right-4">
+            <MdSend />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Id;
